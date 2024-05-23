41a1ba405e54745621be19daf93ab47ff7eb5fe1
StrivingToAdoniss <gorovyi.sv@gmail.com>
2024-05-23 10:15:31 +0300

    Created a NodeJS dummy project(Shrek of Bee Movie script displayer)

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Endpoint to serve the selected script
+app.get('/script', (req, res) => {
+    const scriptName = req.query.name;
+    let filePath;
+
+    switch (scriptName) {
+        case 'bee':
+            filePath = path.join(__dirname, 'public', 'bee_movie_script.txt');
+            break;
+        case 'shrek':
+        default:
+            filePath = path.join(__dirname, 'public', 'shrek_script.txt');
+            break;
+    }
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading script file');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
